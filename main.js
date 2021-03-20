ae37cfabbee8e981cdc1392f0f975407ab69a10a
Unknown <blossomperez18@gmail.com>
2021-03-20 14:04:55 -0500

    Add trivia question object

---
+++ main.js
@@ -0,0 +1,73 @@
+const triviaQuestions = [
+	{
+		question: "",
+		options: {
+			a: "",
+			b: "",
+			c: "",
+			d: ""
+		},
+		answer: ""
+	},
+	{
+		question: "",
+		options: {
+			a: "",
+			b: "",
+			c: "",
+			d: ""
+		},
+		answer: ""
+	},
+	{
+		question: "",
+		options: {
+			a: "",
+			b: "",
+			c: "",
+			d: ""
+		},
+		answer: ""
+	},
+	{
+		question: "",
+		options: {
+			a: "",
+			b: "",
+			c: "",
+			d: ""
+		},
+		answer: ""
+	},
+	{
+		question: "",
+		options: {
+			a: "",
+			b: "",
+			c: "",
+			d: ""
+		},
+		answer: ""
+	},
+	{
+		question: "",
+		options: {
+			a: "",
+			b: "",
+			c: "",
+			d: ""
+		},
+		answer: ""
+	},
+	{
+		question: "",
+		options: {
+			a: "",
+			b: "",
+			c: "",
+			d: ""
+		},
+		answer: ""
+	}
+
+]
