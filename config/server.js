0e2cc54c5333bd7a795106aa4771f489e7919e30
Unknown <blossomperez18@gmail.com>
2021-04-06 13:35:54 -0500

    Add config for production database

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5211ff14aa18712a177b742419a843e9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5e0b8e19-70f0-4a4d-af13-95b5e69f9859'
+};
