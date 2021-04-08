d2ede69108bc5c33e3979ee62c3b96dbe3f40209
Unknown <blossomperez18@gmail.com>
2021-04-07 21:29:36 -0500

    Alternate config for production

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,6 +1,3 @@
-const { parse } = require("pg-connection-string");
-const config = parse(process.env.DATABASE_URL);
-
 module.exports = ({ env }) => {
   if(env("NODE_ENV") === "production") {
     return {
@@ -10,11 +7,11 @@ module.exports = ({ env }) => {
           connector: "bookshelf",
           settings: {
             client: "postgres",
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.username,
-            password: config.password,
+            host: env("DATABASE_HOST", "127.0.0.1"),
+            port: env.int("DATABASE_PORT", 27017),
+            database: env("DATABASE_NAME", "strapi"),
+            username: env("DATABASE_USERNAME", ""),
+            password: env("DATABASE_PASSWORD", ""),
             ssl: { rejectUnauthorized: false }
           },
           options: {
